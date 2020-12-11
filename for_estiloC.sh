19f9ed1cb9ecf577024f3fd56a7c51e7ce9c5e13
rodrigo diego <rdieguez750@gmail.com>
2020-12-11 20:12:33 -0300

    scripts basicos

---
+++ for_estiloC.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#exmplo de for estilo C
+
+for(( i=0; i<11 ; i++));do
+   echo "numero $i"
+done
---
+++ ler_paremetros.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#teste parametros linha de comando
+
+quad=$[ $1*$2 ]
+echo "o resultado e $quad"
+echo $#
